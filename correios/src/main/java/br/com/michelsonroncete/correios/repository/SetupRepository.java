d5486c52209924206b9fc8d2d52c584985004a45
michelsonron7 <michelsonroncete@gmail.com>
2023-02-17 18:10:10 -0300

    implementing setup

---
+++ correios/src/main/java/br/com/michelsonroncete/correios/repository/SetupRepository.java
@@ -0,0 +1,52 @@
+package br.com.michelsonroncete.correios.repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Repository;
+
+import br.com.michelsonroncete.correios.model.Address;
+
+@Repository
+public class SetupRepository {
+	
+	@Value("${correios.base.url}")
+	private String url;
+	
+	public List<Address> getFromOrigin() throws Exception {
+		List<Address> resultList = new ArrayList<>();
+		String resultStr = "";
+		
+		try (CloseableHttpClient httpClient = HttpClients.createDefault();
+				CloseableHttpResponse response = httpClient.execute(new HttpGet(this.url))){
+			HttpEntity entity = response.getEntity();
+			resultStr = EntityUtils.toString(entity);
+		}
+		
+		String [] resultStrSplited = resultStr.split("\n");
+		
+		for (String currentLine : resultStrSplited) {
+			String [] currentLineSplited = currentLine.split(",");
+			
+			resultList.add(Address.builder()
+					.state(currentLineSplited[0])
+					.city(currentLineSplited[1])
+					.district(currentLineSplited[2])
+					.zipcode(StringUtils.leftPad(currentLineSplited[3], 8, "0"))
+					.street(currentLineSplited.length > 4 ? currentLineSplited[4] : null)
+					.build());
+		}
+		
+		
+		
+		return resultList;
+	}
+}
--- correios/src/main/java/br/com/michelsonroncete/correios/service/CorreiosService.java
+++ correios/src/main/java/br/com/michelsonroncete/correios/service/CorreiosService.java
@@ -1,8 +1,13 @@
 package br.com.michelsonroncete.correios.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.event.ApplicationStartedEvent;
+import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
 
+import br.com.michelsonroncete.correios.CorreiosApplication;
 import br.com.michelsonroncete.correios.exception.NoContentException;
 import br.com.michelsonroncete.correios.exception.NotReadyException;
 import br.com.michelsonroncete.correios.model.Address;
@@ -10,33 +15,77 @@ import br.com.michelsonroncete.correios.model.AddressStatus;
 import br.com.michelsonroncete.correios.model.Status;
 import br.com.michelsonroncete.correios.repository.AddressRepository;
 import br.com.michelsonroncete.correios.repository.AddressStatusRepository;
+import br.com.michelsonroncete.correios.repository.SetupRepository;
 
 @Service
-public class CorreiosService{
+public class CorreiosService {
+	
+	private static Logger logger = LoggerFactory.getLogger(CorreiosService.class);
+	
 	@Autowired
 	private AddressRepository addressRepository;
-	
+
 	@Autowired
 	private AddressStatusRepository addressStatusRepository;
-	
+
+	@Autowired
+	private SetupRepository setupRepository;
+
 	public Status getStatus() {
 		return this.addressStatusRepository.findById(AddressStatus.DEFAUT_ID)
-				.orElse(AddressStatus.builder().status(Status.NEED_SETUP).build())
-				.getStatus();
-				
+				.orElse(AddressStatus.builder().status(Status.NEED_SETUP).build()).getStatus();
+
 	}
+
 	public Address getAddressByZipcode(String zipcode) throws NoContentException, NotReadyException {
 		if (!this.getStatus().equals(Status.READY))
 			throw new NotReadyException();
-		
+
 		return addressRepository.findById(zipcode)
-				.orElseThrow(NoContentException::new);
-		
-		
+			   .orElseThrow(NoContentException::new);
+
+	}
+
+	private void saveStatus(Status status) {
+		this.addressStatusRepository
+		.save(AddressStatus.builder()
+		.id(AddressStatus.DEFAUT_ID)
+		.status(status).build());
+
 	}
 	
-	public void setup() {
-		
+	@EventListener(ApplicationStartedEvent.class)
+	protected void setupOnStartup() {
+		try {
+			this.setup();
+		}catch(Exception exc) {
+			CorreiosApplication.close(999);
+			logger.error(".setupOnStartup() - Exception", exc);
+		}
 	}
-	
+
+	public void setup() throws Exception {
+		logger.info("------");
+		logger.info("------");
+		logger.info("------ SETUP RUNNING ");
+		logger.info("------");
+		logger.info("------");
+
+		if (this.getStatus().equals(Status.NEED_SETUP)) {
+				this.saveStatus(Status.SETUP_RUNNING);
+			try {
+			this.setupRepository.getFromOrigin();
+			}catch(Exception exc) {
+				this.saveStatus(Status.NEED_SETUP);
+				throw exc;
+			}
+			this.saveStatus(Status.READY);
+		}
+		logger.info("------");
+		logger.info("------");
+		logger.info("------ SERVICE READY ");
+		logger.info("------");
+		logger.info("------");
+	}
+
 }
